59dc01e403756e1a72c7b600e46bd136f0037f28
sarahara-vaulx <sarahjaneaguilar13@gmail.com>
2026-04-24 14:28:10 +0800

    Update products.js

--- products.js
+++ products.js
@@ -26,7 +26,10 @@ function renderProducts(filter = 'all') {
           <div class="product-price">
             ${p.orig ? `<span class="orig">$${p.orig}</span>` : ''}$${p.price}
           </div>
-          <button class="add-btn" onclick="addToCart(${p.id},event)">+</button>
+          <a href="${p.affiliateUrl}" target="_blank" class="amazon-btn">
+            <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M18 13v6a2 2 0 01-2 2H5a2 2 0 01-2-2V8a2 2 0 012-2h6"/><polyline points="15 3 21 3 21 9"/><line x1="10" y1="14" x2="21" y2="3"/></svg>
+            Buy on Amazon
+          </a>
         </div>
       </div>
     </div>`).join('');
